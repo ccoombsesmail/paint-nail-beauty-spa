f9a8b469cedc7e75f33c412c8397b17b962605c3
Charles <coombs.charles@gmail.com>
2024-02-19 23:54:17 -0800

    remove migrations

--- app/database/migrations/20240218110150_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `franchise_code` to the `employees` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "employees" ADD COLUMN     "franchise_code" TEXT NOT NULL;
