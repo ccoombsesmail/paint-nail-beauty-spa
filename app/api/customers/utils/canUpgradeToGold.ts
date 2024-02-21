8b2e7d4806d1dc0c2a5416fbae497c952f7392a4
Charles <coombs.charles@gmail.com>
2024-02-21 03:03:18 -0800

    Fix some issues

--- app/api/customers/utils/canUpgradeToGold.ts
+++ app/api/customers/utils/canUpgradeToGold.ts
@@ -4,14 +4,14 @@ export function canUpgradeToGold(customer: Customer): boolean {
   // Check if the user is currently Bronze or Silver
   const isEligibleMembership = customer.membershipLevel.toString() === $Enums.Membership.Bronze || customer.membershipLevel.toString() === $Enums.Membership.Silver;
 
-  if (!customer.membershipStartDate) return true
+  if (!customer.membershipPurchaseDate) return true
   if (!isEligibleMembership || customer.membershipLevel.toString() === $Enums.Membership.Gold) return false
 
 
   // Calculate the difference in days between now and the membership start date
   const now = new Date();
-  const membershipStartDate = new Date(customer.membershipStartDate);
-  const diffTime = Math.abs(now.getTime() - customer.membershipStartDate.getTime());
+  const membershipStartDate = new Date(customer.membershipPurchaseDate);
+  const diffTime = Math.abs(now.getTime() - customer.membershipPurchaseDate.getTime());
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
   // Check if the membership started within the last 60 days
