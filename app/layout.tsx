5452ce67ad88a02e3acdfd05399109c853f10e7d
Charles <coombs.charles@gmail.com>
2024-02-24 03:33:29 -0800

    trying

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import './globals.css';
 import "primereact/resources/themes/lara-light-pink/theme.css";
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css';
-import 'react-toastify/dist/ReactToastify.css';
+import './globals.css';
+
 
 import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
