4f8971fe1bfa2bb75023c8cb0508e4af5db7638b
Charles <coombs.charles@gmail.com>
2024-02-24 03:12:43 -0800

    use base url

--- app/client-api/axios.ts
+++ app/client-api/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 // Create an instance of axios
-console.log(process.env.NEXT_PUBLIC_API_BASE_URL)
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: window.location.origin,
   // You can add more default settings here
 });
 
