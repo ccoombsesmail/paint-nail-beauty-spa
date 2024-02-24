f667b46e416fc12096406ed79f41f694e02a77cb
Charles <coombs.charles@gmail.com>
2024-02-24 05:23:14 -0800

    fix base url

--- app/client-api/axios.ts
+++ app/client-api/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // Create an instance of axios
-const baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:3000/api" : `${window.location.origin}/api`
+// const baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:3000/api" : `${window.location.origin}/api`
 const axiosClient = axios.create({
-  baseURL
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   // You can add more default settings here
 });
 
