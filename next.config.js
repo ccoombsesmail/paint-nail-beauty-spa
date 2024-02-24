d17a9c8bdea0eeab05abfbfed81900c4d885315f
Charles <coombs.charles@gmail.com>
2024-02-24 03:36:47 -0800

    trying next config change

--- next.config.js
+++ next.config.js
@@ -11,7 +11,9 @@ const nextConfig = {
         hostname: 'avatar.vercel.sh'
       }
     ]
-  }
+  },
+  pageExtensions: process.env["CC_SITE_ID"] ? ["static.tsx"] : ["server.tsx"],
+
 };
 
 module.exports = nextConfig;
