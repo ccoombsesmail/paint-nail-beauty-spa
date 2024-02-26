ad190e14d4096c6334261d6816013252f257f0ea
Charles <coombs.charles@gmail.com>
2024-02-26 03:03:51 -0800

    make public to checkout something

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,60 @@
-"use node"
-
-import { authMiddleware, redirectToSignIn  } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from 'next/server';
-import { decodeJwt } from 'jose';
-
-
 
 export default authMiddleware({
-  publicRoutes: ["/api/clerk", "/api/seed", '/not-authorized'],
+  publicRoutes: ["/"], // Set all routes as public
   async afterAuth(auth, req, evt) {
-    // Handle users who aren't authenticated
-
-    if (req.nextUrl.pathname === '/not-authorized') {
-      return NextResponse.next();
-    }
-
-    const token = await auth.getToken({ template: 'franchise_code'})
-
-    if (token) {
-      const payload = decodeJwt(token)
-      console.log(req.nextUrl.origin)
-      if (!payload.franchise_code && req.nextUrl.href !== `${req.nextUrl.origin}/not-authorized`) {
-        return NextResponse.redirect( `${req.nextUrl.origin}/not-authorized` );
-      }
-
-    }
-
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // If the user is logged in and trying to access a protected route, allow them to access route
-    if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-    // Allow users visiting public routes to access them
+    // Allow access to all routes without requiring authentication
     return NextResponse.next();
   },
 });
 
-
-
-
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
 };
+
+
+// import { authMiddleware, redirectToSignIn  } from "@clerk/nextjs";
+// import { NextResponse } from 'next/server';
+// import { decodeJwt } from 'jose';
+//
+//
+//
+// export default authMiddleware({
+//   publicRoutes: ["/api/clerk", "/api/seed", '/not-authorized'],
+//   async afterAuth(auth, req, evt) {
+//     // Handle users who aren't authenticated
+//
+//     if (req.nextUrl.pathname === '/not-authorized') {
+//       return NextResponse.next();
+//     }
+//
+//     const token = await auth.getToken({ template: 'franchise_code'})
+//
+//     if (token) {
+//       const payload = decodeJwt(token)
+//       console.log(req.nextUrl.origin)
+//       if (!payload.franchise_code && req.nextUrl.href !== `${req.nextUrl.origin}/not-authorized`) {
+//         return NextResponse.redirect( `${req.nextUrl.origin}/not-authorized` );
+//       }
+//
+//     }
+//
+//     if (!auth.userId && !auth.isPublicRoute) {
+//       return redirectToSignIn({ returnBackUrl: req.url });
+//     }
+//     // If the user is logged in and trying to access a protected route, allow them to access route
+//     if (auth.userId && !auth.isPublicRoute) {
+//       return NextResponse.next();
+//     }
+//     // Allow users visiting public routes to access them
+//     return NextResponse.next();
+//   },
+// });
+//
+//
+//
+//
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+// };
