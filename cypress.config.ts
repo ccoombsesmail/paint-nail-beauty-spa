5ca1dcbe013e09b1d214bdecf26afcf50d582de9
Charles <coombs.charles@gmail.com>
2024-03-03 17:51:17 -0800

    Try to make circelci work

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ import prismaClient from './app/database/prismaClient';
 export default defineConfig({
   projectId: "au998x",
   e2e: {
+    baseUrl: "http://localhost:3000",
+    supportFile: "cypress/support/e2e.ts",
     setupNodeEvents(on, config) {
       on("task", {
         async clearDB() {
